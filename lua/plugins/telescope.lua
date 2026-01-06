69503ceda20f599d6b737b3c633accb8e52e62ef
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2026-01-05 19:13:33 -0800

    fix

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,5 +2,10 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      defaults = {
+        file_ignore_patterns = { "golden", "%.golden" },
+      },
+    },
   },
 }
